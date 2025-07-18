5262f9fe041713b3eba2e23819b83af72ffa7ca5
msw2004727 <msw741121@gmail.com>
2025-07-18 16:04:30 +0800

    Add files via upload

---
+++ config/firebase.js
@@ -0,0 +1,16 @@
+// backend/config/firebase.js
+const admin = require('firebase-admin');
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+});
+
+module.exports = admin;
